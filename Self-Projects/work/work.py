49c2464661772d1dc8aed6dc5a28a98e98b014b5
Sharan Shetty <73932271+Sharzzz001@users.noreply.github.com>
2025-06-12 13:51:15 +0530

    Update work.py

--- Self-Projects/work/work.py
+++ Self-Projects/work/work.py
@@ -1,9 +1,10 @@
 Calendar = 
-ADDCOLUMNS(
-    CALENDAR(DATE(2022,1,1), DATE(2026,12,31)),
-    "IsWorkingDay", 
-        IF (
-            WEEKDAY([Date], 2) <= 5,   -- 2 = Monday, 7 = Sunday
+ADDCOLUMNS (
+    CALENDAR(DATE(2022, 1, 1), DATE(2026, 12, 31)),
+    "IsWorkingDay",
+        VAR ThisDate = [Date]
+        RETURN IF (
+            WEEKDAY(ThisDate, 2) <= 5,  -- 2 = Monday, 6/7 = weekend
             TRUE(),
             FALSE()
         )

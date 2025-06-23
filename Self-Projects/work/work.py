71476e3aa9fb75a91dd25dbfc6c950db41dbbfde
Sharan Shetty <73932271+Sharzzz001@users.noreply.github.com>
2025-06-23 13:35:16 +0530

    Update work.py

--- Self-Projects/work/work.py
+++ Self-Projects/work/work.py
@@ -701,3 +701,28 @@ RETURN
 
         FALSE()
     )
+    
+    
+SLA Due DateTime = 
+VAR StartDate = [Adjusted Start Date]
+VAR SLA_Days = 'Table'[SLA_Days]
+
+VAR BusinessDays =
+    FILTER (
+        'Calendar',
+        'Calendar'[Date] >= StartDate &&
+        'Calendar'[IsBusinessDay] = TRUE
+    )
+
+VAR TargetDate =
+    MAXX (
+        TOPN ( SLA_Days, BusinessDays, 'Calendar'[Date], ASC ),
+        'Calendar'[Date]
+    )
+
+RETURN
+    IF (
+        NOT ISBLANK(TargetDate),
+        TargetDate + TIME(23, 59, 59),
+        BLANK()
+    )

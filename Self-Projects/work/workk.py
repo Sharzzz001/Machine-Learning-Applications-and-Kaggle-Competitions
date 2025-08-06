91b49a41af2771a80b9037f0aa45bf743671fe9f
Sharan Shetty <73932271+Sharzzz001@users.noreply.github.com>
2025-08-06 08:54:38 +0530

    Update workk.py

--- Self-Projects/work/workk.py
+++ Self-Projects/work/workk.py
@@ -6,3 +6,12 @@ SWITCH(
     'YourTable'[Age_To_Completion] <= 90, "61–90 Days",
     "90+ Days"
 )
+
+Due_Age_Bucket_Sort =
+SWITCH(
+    TRUE(),
+    'YourTable'[Due_Age_Bucket] = "0–30 Days", 1,
+    'YourTable'[Due_Age_Bucket] = "31–60 Days", 2,
+    'YourTable'[Due_Age_Bucket] = "61–90 Days", 3,
+    4
+)

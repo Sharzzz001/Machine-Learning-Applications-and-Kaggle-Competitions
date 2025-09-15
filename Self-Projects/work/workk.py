d5a7e4d2d477f9bc965ebd3c238e54246ff0113c
Sharan Shetty <73932271+Sharzzz001@users.noreply.github.com>
2025-09-16 04:24:12 +0530

    Update workk.py

--- Self-Projects/work/workk.py
+++ Self-Projects/work/workk.py
@@ -1,20 +1,11 @@
-# To (RM)
-rm_email = str(group['RM'].iloc[0]).strip() if pd.notnull(group['RM'].iloc[0]) else None
-
-# Build CC list safely
-cc_addresses = []
-if "YES" in group['Escalation TH'].values and pd.notnull(group['Team Head'].iloc[0]):
-    cc_addresses.append(str(group['Team Head'].iloc[0]).strip())
-if "YES" in group['Escalation GH'].values and pd.notnull(group['Group Head'].iloc[0]):
-    cc_addresses.append(str(group['Group Head'].iloc[0]).strip())
-
-# Deduplicate and remove RM if also in CC
-cc_addresses = list(set([a for a in cc_addresses if a]))
-if rm_email in cc_addresses:
-    cc_addresses.remove(rm_email)
-
-# Assign addresses
-if rm_email:
-    mail.To = rm_email
-if cc_addresses:
-    mail.CC = ";".join(cc_addresses)
+Report Label =
+VAR TodayDate = TODAY()
+VAR FirstOfMonth = DATE(YEAR(TodayDate), MONTH(TodayDate), 1)
+VAR EndPrevMonth = EOMONTH(TodayDate, -1)
+VAR FifteenthThisMonth = DATE(YEAR(TodayDate), MONTH(TodayDate), 15)
+RETURN
+    IF(
+        DAY(TodayDate) <= 15,
+        "Report as of " & FORMAT(EndPrevMonth, "DD MMMM"),
+        "Report as of " & FORMAT(FifteenthThisMonth, "DD MMMM")
+    )

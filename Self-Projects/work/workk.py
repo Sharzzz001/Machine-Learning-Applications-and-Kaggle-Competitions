ccef2db7ad44c09b3cd7a62a7b8b4ea25a10f829
Sharan Shetty <73932271+Sharzzz001@users.noreply.github.com>
2025-09-16 20:01:31 +0530

    Update workk.py

--- Self-Projects/work/workk.py
+++ Self-Projects/work/workk.py
@@ -1,16 +1,34 @@
-// Individual day columns (completed OR cancelled)
-NOT IsTotalRow && NOT ISBLANK(SelectedDate),
-    CALCULATE(
-        COUNTROWS(RR_Table),
-        (
-            ( RR_Table[CompletionDate] = SelectedDate
-              && RR_Table[StatusCorpInd] = "KYC Completed"
+ROW (
+    "Team", "Account Opening",
+    "Process", "Document Review (VIP/Non-VIP)",
+    "SLA_Percentage", [SLA_DocumentReview_VIP],
+    "Comments",
+        IF (
+            ISBLANK([SLA_DocumentReview_VIP]),
+            "No cases",
+            IF (
+                [SLA_DocumentReview_VIP] > 0.8,
+                "-",
+                "Your static comment here"
             )
-            ||
-            ( RR_Table[ClosureDate] = SelectedDate
-              && RR_Table[RR_Status] = "Cancelled"
-            )
-        ),
-        RR_Table[Flag_Due] = TRUE()
-            || RR_Table[Flag_Due_Next] = TRUE()
-    )
+        )
+)
+
+
+CALCULATE (
+            COUNTROWS ( RR_Table ),
+            (
+                ( MONTH ( RR_Table[CompletionDate] ) = MONTH ( TODAY() )
+                  && YEAR ( RR_Table[CompletionDate] ) = YEAR ( TODAY() )
+                  && RR_Table[StatusCorpInd] = "KYC Completed"
+                )
+                ||
+                ( MONTH ( RR_Table[ClosureDate] ) = MONTH ( TODAY() )
+                  && YEAR ( RR_Table[ClosureDate] ) = YEAR ( TODAY() )
+                  && RR_Table[RR_Status] = "Cancelled"
+                )
+            ),
+            RR_Table[Flag_Due] = TRUE()
+                || RR_Table[Flag_Due_Next] = TRUE()
+        )
+)

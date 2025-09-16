8d7f4d75c7e359b0fd5f57bf87c10665dc31a452
Sharan Shetty <73932271+Sharzzz001@users.noreply.github.com>
2025-09-16 19:58:24 +0530

    Update workk.py

--- Self-Projects/work/workk.py
+++ Self-Projects/work/workk.py
@@ -1,15 +1,16 @@
-ROW (
-    "Team", "Account Opening",
-    "Process", "Document Review (VIP/Non-VIP)",
-    "SLA_Percentage", [SLA_DocumentReview_VIP],
-    "Comments",
-        IF (
-            ISBLANK([SLA_DocumentReview_VIP]),
-            "No cases",
-            IF (
-                [SLA_DocumentReview_VIP] > 0.8,
-                "-",
-                "Your static comment here"
+// Individual day columns (completed OR cancelled)
+NOT IsTotalRow && NOT ISBLANK(SelectedDate),
+    CALCULATE(
+        COUNTROWS(RR_Table),
+        (
+            ( RR_Table[CompletionDate] = SelectedDate
+              && RR_Table[StatusCorpInd] = "KYC Completed"
             )
-        )
-)
+            ||
+            ( RR_Table[ClosureDate] = SelectedDate
+              && RR_Table[RR_Status] = "Cancelled"
+            )
+        ),
+        RR_Table[Flag_Due] = TRUE()
+            || RR_Table[Flag_Due_Next] = TRUE()
+    )

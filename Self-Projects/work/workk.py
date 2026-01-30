6089260d178be2c6d34d598fee8f57577ec16a15
Sharan Shetty <73932271+Sharzzz001@users.noreply.github.com>
2026-01-30 13:13:33 +0530

    Update workk.py

--- Self-Projects/work/workk.py
+++ Self-Projects/work/workk.py
@@ -1,1 +1,3 @@
 cmd.exe /k ""Z:\Shared_Drive\MyTool\venv\Scripts\python.exe" "Z:\Shared_Drive\MyTool\my_script.py""
+
+cmd.exe /c ""Z:\Corporate Folder\My Tool\venv\Scripts\python.exe" "Z:\Corporate Folder\My Tool\my_script.py""
